65388e98de0c846be87bde3fb03e7590dadf7172
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2024-07-31 10:10:18 -0600

    Work on readline like editing.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
-set -o nounset # Fails when accessing an unset variable.
-set -o errexit # Exits if a command exits with a non-zero status.
-
 STTY=`stty -g`
 
-cd src && \
-../etc/uxnasm merlin/includes.tal ../rom/merlin.rom $@ && \
-cd .. && \
+# Build
+cd src
+../etc/uxnasm merlin/includes.tal ../rom/merlin.rom $@ || exit 127
+cd ..
 
+# Run
 stty raw -echo
-uxncli rom/merlin.rom
+uxncli rom/merlin.rom || EXIT=`echo $?`
+
+# Exit
 stty $STTY
+exit $EXIT
