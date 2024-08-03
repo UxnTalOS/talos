d05a2337a0af46282e7ee46a1255f9cecb4a7452
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2024-08-03 17:18:52 -0600

    Test braketed paste mode.

--- etc/ansi.sh
+++ etc/ansi.sh
@@ -6,6 +6,6 @@ uxnasm etc/ansi.tal etc/ansi.rom
 
 stty raw -echo    # non canonical mode and no echo
 
-uxncli etc/ansi.rom
+echo "\x1b[?2004h" && uxncli etc/ansi.rom
 
 stty $TTY    # restore tty settings
