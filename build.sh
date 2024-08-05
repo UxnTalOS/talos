3a33f3127483dd56152697887ff6e274e155149e
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2024-08-05 09:59:36 -0600

    Translate \n to \r\n on output.

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ STTY=`stty -g`
 
 
 # Pre-process
-if [ "$1" = "DEBUG" ]; then
+if [ "$1" = "--debug" ]; then
 	DEBUG="DBG"
 else
 	DEBUG="NO_DBG"
 fi
 
-cpp -P -D $DEBUG config/pre-options.tal -o config/options.tal
-cpp -P -D $DEBUG src/debugger/routines/pre-after-eval.tal \
+cpp -P -w -D $DEBUG config/pre-options.tal -o config/options.tal
+cpp -P -w -D $DEBUG src/debugger/routines/pre-after-eval.tal \
 -o src/debugger/routines/after-eval.tal
-cpp -P -D $DEBUG src/debugger/routines/pre-before-eval.tal \
+cpp -P -w -D $DEBUG src/debugger/routines/pre-before-eval.tal \
 -o src/debugger/routines/before-eval.tal
 
 
@@ -25,10 +25,18 @@ cd src
 ../etc/uxnasm talos/includes.tal ../rom/talos.rom || exit 127
 cd ..
 
+# Install
+if [ "$1" = "--install" ]; then
+	echo "Installing ./{bin,rom} at ~/{bin,roms}"
+	cp bin/* ~/bin
+	cp rom/talos.rom ~/roms
+fi
+
 
 # Run
-stty raw -echo
-uxncli rom/talos.rom
+stty -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon \
+     -ixoff -icanon onlcr -echo -isig -iuclc -ixany -imaxbel -xcase min 1 time 0
+uxn38 -n rom/talos.rom
 EXIT=`echo $?`
 
 
