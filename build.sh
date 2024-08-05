1d36b196430651f919a6856ee4075c884c4d399d
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2024-08-04 20:19:40 -0600

    Use C preprocessor for conditional debugging.

--- build.sh
+++ build.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env sh
 
+# Start
 STTY=`stty -g`
+
+
+# Pre-process
+if [ "$1" = "DEBUG" ]; then
+	DEBUG="DBG"
+else
+	DEBUG="NO_DBG"
+fi
+
+cpp -P -D $DEBUG config/pre-options.tal -o config/options.tal
+cpp -P -D $DEBUG src/debugger/routines/pre-after-eval.tal \
+-o src/debugger/routines/after-eval.tal
+cpp -P -D $DEBUG src/debugger/routines/pre-before-eval.tal \
+-o src/debugger/routines/before-eval.tal
+
 
 # Build
 cd src
 ../etc/uxnasm talos/includes.tal ../rom/talos.rom || exit 127
 cd ..
 
+
 # Run
 stty raw -echo
 uxncli rom/talos.rom
+
 
 # Exit
  EXIT=`echo $?`
